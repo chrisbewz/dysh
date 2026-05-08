ac3a327f3c9f3d674a5c5e4db5973ba7570535f1
chrisbewz <crisbewz@gmail.com>
2026-05-08 03:19:06 -0300

    feat: add AppVeyor configuration files for CI/CD setup

---
+++ build/Build.AppVeyor.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2023 Maintainers of NUKE.
+// Distributed under the MIT License.
+// https://github.com/nuke-build/nuke/blob/master/LICENSE
+
+using Nuke.Common.CI.AppVeyor;
+
+[AppVeyor(
+    suffix: null,
+    AppVeyorImage.VisualStudio2022,
+    BranchesOnly = [MasterBranch, $"/{ReleaseBranchPrefix}\\/*/"],
+    SkipTags = true,
+    InvokedTargets = [nameof(IPack.Pack), nameof(ITest.Test), nameof(IPublish.Publish)],
+    Secrets =
+    [
+        nameof(PublicNuGetApiKey)
+    ])]
+[AppVeyor(
+    suffix: "continuous",
+    AppVeyorImage.VisualStudioLatest,
+    AppVeyorImage.UbuntuLatest,
+    AppVeyorImage.MacOsLatest,
+    BranchesExcept = [MasterBranch, $"/{ReleaseBranchPrefix}\\/*/"],
+    SkipTags = true,
+    InvokedTargets = [nameof(ITest.Test), nameof(IPack.Pack)],
+    Secrets = [])]
+partial class Build
+{
+}
